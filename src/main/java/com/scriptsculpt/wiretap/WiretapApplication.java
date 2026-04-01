8a42168a1a1675168d99a703f3a7c57a1f8599ff
Aastha Singh <singhaastha1202@gmail.com>
2026-04-01 21:06:16 +0530

    Initial setup of Wiretap project

---
+++ src/main/java/com/scriptsculpt/wiretap/WiretapApplication.java
@@ -0,0 +1,13 @@
+package com.scriptsculpt.wiretap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiretapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiretapApplication.class, args);
+	}
+
+}
