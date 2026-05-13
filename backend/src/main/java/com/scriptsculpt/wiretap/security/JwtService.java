f549eeef029f78046c8a9c0b4174802a4ff535b2
Aastha Singh <singhaastha1202@gmail.com>
2026-05-13 19:25:32 +0530

    Jwt Authentication and authorization on the protected apis addition

--- backend/src/main/java/com/scriptsculpt/wiretap/security/JwtService.java
+++ backend/src/main/java/com/scriptsculpt/wiretap/security/JwtService.java
@@ -1,17 +1,33 @@
 package com.scriptsculpt.wiretap.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
-    private static final String JWT_SECRET_KEY = "jwtsecretyeywith12";
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, claims -> claims.getSubject());
+    }
 
     public String generateToken(String username) {
         return Jwts.builder()
@@ -23,7 +39,27 @@ public class JwtService {
     }
 
     private Key getSignKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(JWT_SECRET_KEY);
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date expiration = extractClaim(token, claims -> claims.getExpiration());
+
+        return expiration.before(new Date());
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
