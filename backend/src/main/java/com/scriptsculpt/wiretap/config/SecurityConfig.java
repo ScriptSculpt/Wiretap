72d4aa1830051aea304da0b0d79351efa98297ea
Aastha Singh <singhaastha1202@gmail.com>
2026-05-16 09:32:12 +0530

    Frontend containerized

--- backend/src/main/java/com/scriptsculpt/wiretap/config/SecurityConfig.java
+++ backend/src/main/java/com/scriptsculpt/wiretap/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowCredentials(true);
